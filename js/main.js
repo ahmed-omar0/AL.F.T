70a85b7e2e691ae6c0e9dcd81a33f4162a7c9209
Ahmed-Alfarouq <mediocre23534@gmail.com>
2021-09-20 04:46:14 -0400

    Refact Files

--- js/main.js
+++ js/main.js
@@ -1,57 +1,61 @@
 /*Start vanbar*/
-let  navbar = document.getElementById("nav");
-document.onscroll = function (){
-    if(window.pageYOffset >= 50){
+let  navbar = document.getElementById("navbar");
+
+document.addEventListener('scroll', () => {
+    if(window.pageYOffset >= 120){
         navbar.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
     }else{
         navbar.style.backgroundColor = "rgb(0, 0, 0)";
     }
-};
-/*End vanbar*/
+});
+
+
 /*Start tabs*/
-let  tab1     = document.getElementById("tab1"),
-    tab2     = document.getElementById("tab2"),
-    tab3     = document.getElementById("tab3"),
-    tab1Text = document.getElementById("tab1-text"),
-    tab2Text = document.getElementById("tab2-text"),
-    tab3Text = document.getElementById("tab3-text");
-tab1.onclick = function (){
+let tab1     = document.getElementById("tab1");
+let tab2     = document.getElementById("tab2");
+let tab3     = document.getElementById("tab3");
+let tab1Text = document.getElementById("tab1-text");
+let tab2Text = document.getElementById("tab2-text");
+let tab3Text = document.getElementById("tab3-text");
+
+tab1.addEventListener('click', () => {
     tab1.classList.add("active");
     tab2.classList.remove("active");
     tab3.classList.remove("active");
     tab1Text.classList.remove("hidden-text");
     tab2Text.classList.add("hidden-text");
     tab3Text.classList.add("hidden-text");
-};
-tab2.onclick = function (){
+});
+
+tab2.addEventListener('click', () => {
     tab2.classList.add("active");
     tab1.classList.remove("active");
     tab3.classList.remove("active");
     tab1Text.classList.add("hidden-text");
     tab3Text.classList.add("hidden-text");
     tab2Text.classList.remove("hidden-text");
-};
-tab3.onclick = function (){
+});
+
+tab3.addEventListener('click', () => {
     tab3.classList.add("active");
     tab2.classList.remove("active");
     tab1.classList.remove("active");
     tab3Text.classList.remove("hidden-text");
     tab2Text.classList.add("hidden-text");
     tab1Text.classList.add("hidden-text");
-};
-/*End tabs*/
-/*Start Up*/
-let  btn = document.getElementById("up");
-window.onscroll = function () {
-    "use strict";
+});
+
+/*Start Scroll Up*/
+let  scrollUp = document.getElementById("up");
+
+window.addEventListener('scroll', () => {
     if(window.pageYOffset >= 1000){
-        btn.style.display ="block";
-    }else{
-        btn.style.display ="none";
+        scrollUp.classList.remove("d-none");
+    } else {
+        scrollUp.classList.add("d-none");
     }
-}
-btn.onclick = function () {
-    "use strict";
-    window.scrollTo(0,0);
-}
-/*End Up*/
+});
+
+scrollUp.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+});
